19fbd21495859bce3c8a8284e01a067be1aace3c
suyash101101 <naharsuyash@gmail.com>
2025-03-22 14:11:30 +0530

    react bits setup

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 import './App.css'
+  import Iridescence from './blocks/Backgrounds/Iridescence/Iridescence.tsx';
 
 function App() {
 
@@ -6,6 +7,12 @@ function App() {
     <>
       <div className="flex justify-center items-center h-screen">
         <h1 className="text-7xl font-bold text-green-500">blockchAIn</h1>
+        <Iridescence
+          color={[1, 1, 1]}
+          mouseReact={false}
+          amplitude={0.1}
+          speed={1.0}
+        />
       </div>
     </>
   )
