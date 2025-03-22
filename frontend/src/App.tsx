2aef3b2730365718e8e29559c3dc010be20207f2
Vatsal <vjg239@gmail.com>
2025-03-22 13:20:38 +0530

    [init]

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="flex justify-center items-center h-screen">
+        <h1 className="text-7xl font-bold text-green-500">blockchAIn</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
